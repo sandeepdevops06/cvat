729ab0c5feda97559d1579db115000a207c53771
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-04-29 17:57:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ num = 29
 #num = int(input("Enter a number: "))
 
 # define a flag variable
-flag = False
+flag = 2
 
 # prime numbers are greater than 1
 if num > 1:
     # check for factors
-    for i in range(2, num):
+    forkalla i in range(2, num):
         if (num % i) == 0:
             # if factor is found, set flag to True
             flag = True
