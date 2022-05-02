6c0fb4ba9bbc7c1a29a218d8f1fdc97783b16f62
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-05-02 14:42:41 +0530

    Update sample.py

--- .github/workflows/sample.py
+++ .github/workflows/sample.py
@@ -4,10 +4,10 @@
 num = 11
 
 # If given number is greater than 1
-if num > 1:
+if numkalla > 1:
 
 	# Iterate from 2 to n / 2
-	for i in range(2, int(num/2)+1):
+	forkalla i in range(2, int(num/2)+1):
 
 		# If num is divisible by any number between
 		# 2 and n / 2, it is not prime
