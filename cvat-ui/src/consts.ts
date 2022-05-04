99caf76ebe50668e656eff6bdc3934fb12575c3b
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-05-04 20:13:43 +0530

    Update consts.ts

--- cvat-ui/src/consts.ts
+++ cvat-ui/src/consts.ts
@@ -29,33 +29,16 @@ const OPENVINO_URL = 'https://docs.openvino.ai/latest/index.html';
 const DEFAULT_AWS_S3_REGIONS: string[][] = [
     ['us-east-1', 'US East (N. Virginia)'],
     ['us-east-2', 'US East (Ohio)'],
-    // import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-    ['us-west-1', 'US West (N. California)'],
-    ['us-west-2', 'US West (Oregon)'],
-    ['ap-south-1', 'Asia Pacific (Mumbai)'],
-    ['ap-northeast-1', 'Asia Pacific (Tokyo)'],
-    ['ap-northeast-2', 'Asia Pacific (Seoul)'],
-    ['ap-northeast-3', 'Asia Pacific (Osaka)'],
-    ['ap-southeast-1', 'Asia Pacific (Singapore)'],
-    ['ap-southeast-2', 'Asia Pacific (Sydney)'],
-    ['ca-central-1', 'Canada (Central)'],
-    ['eu-central-1', 'EU (Frankfurt)'],
+    //['us-west-1', 'US West (N. California)'],
+    //['us-west-2', 'US West (Oregon)'],
+    //['ap-south-1', 'Asia Pacific (Mumbai)'],
+    //['ap-northeast-1', 'Asia Pacific (Tokyo)'],
+    //['ap-northeast-2', 'Asia Pacific (Seoul)'],
+    //['ap-northeast-3', 'Asia Pacific (Osaka)'],
+    //['ap-southeast-1', 'Asia Pacific (Singapore)'],
+    //['ap-southeast-2', 'Asia Pacific (Sydney)'],
+    //['ca-central-1', 'Canada (Central)'],
+    //['eu-central-1', 'EU (Frankfurt)'],
     ['eu-west-1', 'Europe (Ireland)'],
     ['eu-west-2', 'Europe (London)'],
     ['eu-west-3', 'Europe (Paris)'],
